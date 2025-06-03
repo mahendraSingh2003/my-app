c966c7801e3fc159a3c82a32cbff41925a90e118
Mahendra Singh Gurjar <mhndrmahendragurjar@gmail.com>
2025-06-03 12:17:28 +0530

    adding home page code in app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 
-
-export default function Home() {
-  // return (
-    
-  // );
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Welcome to My Website</h1>
+      <p>This is the homepage. Explore the About, Services, and Contact pages.</p>
+    </div>
+  )
 }
